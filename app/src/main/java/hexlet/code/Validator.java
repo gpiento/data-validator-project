92419cafede9ce90b2e825f4402a7f40a1c48afc
gpiento <zzzest@gmail.com>
2024-03-06 20:58:40 +0300

    change Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -4,11 +4,11 @@ public class Validator {
 
     public static void main(String[] args) {
 
-        var v = new Validator();
-        // Проверки накапливаются в схеме, а не заменяют друг друга
-        var schema = v.string().required().minLength(5).contains("hex");
+        Validator v = new Validator();
+        StringSchema schema = v.string().required().minLength(5).contains("hex");
         schema.isValid("hello"); // false
         schema.isValid("hexlet"); // true}
+    }
 
     public StringSchema string() {
 
