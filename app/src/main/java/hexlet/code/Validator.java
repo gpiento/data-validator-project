f6a8de6679586a8c29fde411a3a46e58b162a12f
gpiento <zzzest@gmail.com>
2024-04-07 02:30:29 +0300

    change Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,32 +1,36 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
 
-    public static void main(String[] args) {
-
-        Validator v = new Validator();
-        BaseSchema<String> schema = v.string().required().minLength(5).contains("hex");
-        System.out.println(schema.isValid("")); // false
-        System.out.println(schema.isValid("asdfsdsd")); // false
-        System.out.println(schema.isValid("asdhexsdf")); // true
-
-    }
-
+    /**
+     * StringSchema.
+     *
+     * @return StringSchema()
+     */
     public StringSchema string() {
 
         return new StringSchema();
     }
 
+    /**
+     * NumberSchema.
+     *
+     * @return NumberSchema()
+     */
     public NumberSchema number() {
 
         return new NumberSchema();
     }
 
+    /**
+     * MapSchema.
+     *
+     * @return MapSchema()
+     */
     public MapSchema map() {
 
         return new MapSchema();
