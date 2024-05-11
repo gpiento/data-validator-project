c054d0039e2ce7b6aefd835331bc7c2a958ffccb
gpiento <zzzest@gmail.com>
2024-05-11 17:26:56 +0300

    change MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,31 +1,26 @@
 package hexlet.code.schemas;
 
 import java.util.Map;
-import java.util.function.Predicate;
+import java.util.Objects;
 
 public class MapSchema extends BaseSchema<Object> {
 
     public final MapSchema required() {
-        Predicate<Map<?, ?>> predicate = m -> m instanceof Map<?, ?>;
-        addPredicate(predicate);
+        addPredicate("required", Objects::nonNull);
         return this;
     }
 
-    public final MapSchema sizeof(int size) {
-
-        Predicate<Map<?, ?>> predicate = m -> m.size() == size;
-        addPredicate(predicate);
+    public final MapSchema sizeof(final int size) {
+        addPredicate("sizeof", map -> ((Map<?, ?>) map).size() == size);
         return this;
     }
 
-    public final <T> MapSchema shape(Map<String, BaseSchema<T>> shape) {
-
-        Predicate<Map<?, ?>> predicate = m -> shape.entrySet().stream().allMatch(e -> {
-            Object value = m.get(e.getKey());
-            BaseSchema schema = e.getValue();
-            return schema.isValid(value);
-        });
-        addPredicate(predicate);
+    public final MapSchema shape(final Map<String, BaseSchema> shape) {
+        addPredicate("shape", map ->
+                shape.entrySet().stream().allMatch(value -> {
+                    Object object = ((Map<?, ?>) map).get(value.getKey());
+                    return value.getValue().isValid(object);
+                }));
         return this;
     }
 }
